a727aae8e3e716e49502241a6398b40616fc7b30
Ilze Kalnina <ilze@skate.lv>
2020-09-02 20:43:43 +0300

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Kisacka on 02/09/2020.
+//  Copyright © 2020 Kisacka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
